107feb0e330edccbd965cc6fca623f32234122bf
Vuk <vuk@Vuks-MacBook-Pro.local>
2026-04-28 21:34:53 +0200

    fix: base path /BookR/

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change '/mmm-reader/' to match your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/mmm-reader/',
+  base: '/BookR/',
 })
